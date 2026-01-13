1279dbea36b301235df337714d1e03be154ede29
SarangaKalhari <kalharisaranga9@gmail.com>
2026-01-13 10:16:40 +0530

    Add basic login UI using JavaFX FXML

---
+++ src/main/java/edu/icet/controller/LoginUIController.java
@@ -0,0 +1,25 @@
+package edu.icet.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginUIController {
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
+
+}
