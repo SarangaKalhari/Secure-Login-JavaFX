40eebbe20c7cbf61dbff716cf8c92b1f3ff710b9
SarangaKalhari <kalharisaranga9@gmail.com>
2026-01-13 13:08:11 +0530

    Add user dto class

---
+++ src/main/java/edu/icet/model/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package edu.icet.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
