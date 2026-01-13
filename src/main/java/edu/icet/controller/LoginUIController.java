0eca5e908e8e8a070cc9af3e56162a8caf94e9af
SarangaKalhari <kalharisaranga9@gmail.com>
2026-01-13 11:08:06 +0530

    Add basic Sign-UP UI using javafx fxml

--- src/main/java/edu/icet/controller/LoginUIController.java
+++ src/main/java/edu/icet/controller/LoginUIController.java
@@ -2,10 +2,17 @@ package edu.icet.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginUIController {
+
+    Stage stage = new Stage();
 
     @FXML
     private TextField txtEmail;
@@ -43,6 +50,13 @@ public class LoginUIController {
 
     @FXML
     void btnSignUpOnAction(ActionEvent event) {
+
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/SignUp.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
---
+++ src/main/java/edu/icet/controller/SignUpUIController.java
@@ -0,0 +1,31 @@
+package edu.icet.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SignUpUIController {
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private TextField txtReEnterEmail;
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginOnAction(ActionEvent event) {
+
+    }
+
+}
