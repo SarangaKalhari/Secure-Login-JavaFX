1c310a4fda292f04812d86beda4bbe1762895803
SarangaKalhari <kalharisaranga9@gmail.com>
2026-01-13 10:56:43 +0530

    implement strong password validation feature

--- src/main/java/edu/icet/controller/LoginUIController.java
+++ src/main/java/edu/icet/controller/LoginUIController.java
@@ -2,6 +2,7 @@ package edu.icet.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class LoginUIController {
@@ -13,16 +14,30 @@ public class LoginUIController {
     private TextField txtPassword;
 
     @FXML
+    private Label txtInvalidEmail;
+
+    @FXML
+    private Label txtInvalidPassword;
+
+    @FXML
     void btnLoginOnAction(ActionEvent event) {
 
         String email = txtEmail.getText().trim();
         String password = txtPassword.getText().trim();
 
-        if (isValidEmail(email)){
-            System.out.println("Valid email Address..");
-        }else {
+        if (isValidEmail(email) && isValidPassword(password)){
+            System.out.println("Valid Login..");
+        }else if (!isValidEmail(email) && isValidPassword(password)){
+            txtInvalidEmail.setText("Invalid Email address.");
             System.out.println("Invalid ..");
+        } else if (isValidEmail(email) && !isValidPassword(password)) {
+            txtInvalidPassword.setText("Invalid Password.");
+
+        }else {
+            txtInvalidEmail.setText("Invalid Email address.");
+            txtInvalidPassword.setText("Invalid Password.");
         }
+
 
     }
 
@@ -34,5 +49,10 @@ public class LoginUIController {
     private boolean isValidEmail(String email){
         String isGmail = "^[a-zA-Z0-9]+@gmail\\.com$";
         return email.matches(isGmail);
+    }
+
+    private boolean isValidPassword(String password){
+        String isPassword = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[^a-zA-Z0-9]).{8,}$";
+        return password.matches(isPassword);
     }
 }
